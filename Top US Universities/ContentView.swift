010cbcae2da74ee3259760ca2a47f52954337e66
tiennguyenskedulo <tien.nguyen@skedulo.com>
2022-07-14 14:45:59 +0700

    Initial Commit

---
+++ Top US Universities/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Top US Universities
+//
+//  Created by Tien Nguyen on 14/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Top US Universities/Top_US_UniversitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Top_US_UniversitiesApp.swift
+//  Top US Universities
+//
+//  Created by Tien Nguyen on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Top_US_UniversitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
